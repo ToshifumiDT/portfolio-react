f00714f19331315679428a1cdd5d521a12fa4275
ToshifumiDT <toshifumi.digitaletalenter@gmail.com>
2024-10-07 11:57:21 +0200

    added about me section

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Header from './components/header/Header';
 import { HamburgerProvider } from './components/header/HamburgerContext';
 import ContactForm from './components/contactform/ContactForm'; 
 import SkillSection from '../src/components/skillicon/SkillSection';
+import About from './components/about/about';
 
 function App() {
   return (
     <HamburgerProvider>
       <Header />
+      <About />
       <SkillSection />
       <ContactForm /> 
       
